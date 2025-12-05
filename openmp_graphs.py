29bc8dc3e18eb9909172b9328f4d2ab21b4ba87c
Kanchan1015 <akanchan0315@gmail.com>
2025-12-05 18:01:45 +0530

    code refactor

--- openmp_graphs.py
+++ openmp_graphs.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 import os
 
-# --- OpenMP measured data ---
+#OpenMP measured data 
 threads = [1, 2, 3, 4, 8]
 times = [0.048822, 0.036610, 0.026622, 0.021934, 0.015739]
 
-# --- Compute speedup ---
+#Compute speedup 
 t1 = times[0]
 speedup = [t1 / t for t in times]
 
-# --- Make output directory ---
+#Make output directory
 out_dir = "OpenMP/screenshots/graphs"
 os.makedirs(out_dir, exist_ok=True)
 
-# --- Time vs Threads ---
+#Time vs Threads 
 plt.figure()
 plt.plot(threads, times, marker='o')
 plt.xlabel('Threads')
@@ -22,7 +22,7 @@ plt.title('OpenMP: Time vs Threads')
 plt.grid(True)
 plt.savefig(f"{out_dir}/OpenMP_time_vs_threads.png", bbox_inches='tight')
 
-# --- Speedup vs Threads ---
+#Speedup vs Threads 
 plt.figure()
 plt.plot(threads, speedup, marker='o')
 plt.xlabel('Threads')
